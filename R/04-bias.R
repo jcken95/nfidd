ad5ac98e32774b4f56ae20aed364ae37ffdb8570
jcken95 <jack_kennedy1@hotmail.co.uk>
2024-07-12 14:21:32 +0100

    feat: ch4 ex 1 complete

---
+++ R/04-bias.R
@@ -0,0 +1,99 @@
+# Bias in delay distributions
+# https://nfidd.github.io/nfidd/sessions/biases-in-delay-distributions.html
+
+# setup ----
+
+source(here::here("R", "00-setup.R"))
+library(tidybayes) #  for exercises
+# load data ----
+
+# simulate infections
+
+source(here("nfidd", "snippets", "onset-hosp.r"))
+
+head(df)
+
+# dates not days ----
+
+# commonly, we don't have the exact infection time. We normally have dates
+# this sensors the observation
+
+# we make the data in df more realistic by rounding the date down to integer
+# in practice, we would work out the number of days between two key dates
+
+df_dates <- mutate(df, across(everything(), floor))
+head(df_dates)
+
+# our data is now double interval sensored
+#  * double: delays represent the time between two events that are both censored
+#  * interval: we only know the time intercal at which the event happened (between 00:00 and 23:59)
+
+
+# estimating delay distributions - accounting for sensoring
+
+# naive approach: ignore the fact that data are censored
+# just compute difference between censored times
+
+df_dates <- df_dates |>
+  mutate(
+    incubation_period = onset_time - infection_time,
+    onset_to_hosp = hosp_time - onset_time
+  )
+
+# Excersise 1 ----
+# Fit the lognormal distribution - are the parameters recovered?
+
+
+mod <- cmdstan_model(here("nfidd", "stan", "lognormal.stan"))
+mod
+# model outline
+# y = time to hospitalisation after symptom first present
+# likelihood:
+# y ~ lognormal(mu, sig)
+# prior:
+# mu ~ normal(0, 10)
+# sig ~ half-normal(0, 10)
+
+stan_data <- drop_na(df_dates, onset_to_hosp)
+## Use the data to sample from the model posterior
+res <- mod$sample(
+  data = list(
+    n = nrow(stan_data),
+    y = stan_data$onset_to_hosp
+  )
+)
+
+# true params:
+# mu  = 1.75 (meanlog)
+# sig = 0.5 (sdlog)
+true_params <- tibble(param = c("meanlog", "sdlog"), value = c(1.75, 0.5))
+true_params_wide <- pivot_wider(true_params,
+                                names_from = "param")
+res$summary()
+
+posterior_samples <- res$draws() |>
+  tidybayes::tidy_draws() |>
+  select(-lp__) |>
+  pivot_longer(
+    cols = c(meanlog, sdlog),
+    values_to = "value",
+    names_to = "param"
+  ) |>
+  left_join(true_params,
+            suffix = c("", "_true"),
+            by = c("param"))
+
+posterior_samples |>
+  ggplot() +
+  geom_histogram(aes(x = value)) +
+  geom_vline(aes(xintercept = value_true)) +
+  facet_wrap(~param, scales = "free")
+
+# perhaps some bias, but the true values are within the (margina) regions of high posterior density
+
+res$draws() |>
+  tidybayes::tidy_draws() |>
+  ggplot(aes(x  = meanlog, y = sdlog)) +
+  geom_point() +
+  geom_density_2d() +
+  geom_point(data = true_params_wide, colour = "red", pch = 15)
