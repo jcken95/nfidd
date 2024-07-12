088ba19e45f2d4ee75e8599ab4c68d9b01dfb128
jcken95 <jack_kennedy1@hotmail.co.uk>
2024-07-12 12:24:00 +0100

    feat: tutorial aspect of ch3

---
+++ R/03-delay_distributions.R
@@ -0,0 +1,150 @@
+# Delay distributions
+# https://nfidd.github.io/nfidd/sessions/delay-distributions.html
+
+# setup ----
+
+library("nfidd")
+library("ggplot2")
+library("dplyr")
+library("tidyr")
+library("lubridate")
+library("here")
+library("cmdstanr")
+library("posterior")
+
+set.seed(123)
+options(cmdstanr_print_line_numbers = TRUE)
+
+# load data ----
+data(infection_times)
+head(infection_times)
+
+### visualise the infection curve
+
+# infection_times are (decimal) values indicating the time at which an individual was
+# infected, relative to the first time of infection
+
+ggplot(infection_times, aes(x = infection_time)) +
+  geom_histogram(binwidth = 1) +
+  scale_x_continuous(n.breaks = 10) +
+  labs(x = "Infection time (in days)", y = "Number of infections",
+       title = "Infections during an outbreak")
+
+# in this case, approx symmetric
+dplyr::summarise(
+  infection_times,
+  mean = mean(infection_time),
+  sd = sd(infection_time),
+  median = median(infection_time),
+  iqr = IQR(infection_time),
+  infection_time |>
+    stats::quantile(probs = c(5, 25, 75, 95)  / 100) |>
+    as.list() |>
+    tibble::as_tibble() |>
+    dplyr::rename_with(
+      \(str) {
+        str_name <- str |>
+          stringr::str_remove("%") |>
+          stringr::str_pad(2, "left", "0")
+        glue::glue(".q{str_name}")
+      }
+    )
+
+)
+
+# simulation of delays after an infection ----
+
+## goal: simulation of hospital arrivals after an outbreak
+
+## assumptions:
+##  * every infection -> symtoms
+##  * incubation period  ~ gamma(shape = 5, rate = 1)
+##  * p(hospitalised | symptoms) = 0.3
+##  * time from symptom onset -> admission ~ lognormal(mu = 1.75, sigma = 0.5)
+
+# Simulate times for symptom onset and hospitalisation for each infection
+
+# remember: onset ~ gamma
+# time to hosp | symptoms present ~ I(hopsitalised) * lognormal + (1 - I(hosptialised)) * NULL
+
+source(
+  here::here("nfidd", "snippets", "onset-hosp.r"),
+  echo = TRUE,
+  max.deparse.length = 1000
+)
+
+# convert our data frame to long format
+dfl <- df |>
+  pivot_longer(
+    cols = c(infection_time, onset_time, hosp_time),
+    names_to = "type", values_to = "time"
+  )
+# plot
+ggplot(dfl, aes(x = time)) +
+  geom_histogram(position = "dodge", binwidth = 1) +
+  facet_wrap(~ type, ncol = 1) +
+  xlab("Time (in days)") +
+  ylab("Count")
+
+# as expected, around 30% of patients are hospitalised
+
+1 - mean(is.na(df$hosp_time))
+
+# estimation of delay distribution from data ----
+
+# aim: recover model parameters (time to hospitalisation)
+
+mod <- cmdstan_model(here("nfidd", "stan", "lognormal.stan"))
+mod
+# model outline
+# y = time to hospitalisation after symptom first present
+# likelihood:
+# y ~ lognormal(mu, sig)
+# prior:
+# mu ~ normal(0, 10)
+# sig ~ half-normal(0, 10)
+
+df_onset_to_hosp <- df |>
+  mutate(onset_to_hosp = hosp_time - onset_time) |>
+  # exclude infections that didn't result in hospitalisation
+  drop_na(onset_to_hosp)
+## Use the data to sample from the model posterior
+res <- mod$sample(
+  data = list(
+    n = nrow(df_onset_to_hosp),
+    y = df_onset_to_hosp$onset_to_hosp
+  )
+)
+
+# true params:
+# mu  = 1.75 (meanlog)
+# sig = 0.5 (sdlog)
+
+res$summary()
+
+# posterior means of these params close to theoretical values
+# E(mu, sig | data) = (1.73, 0.494)
+
+## get shape and rate samples from the posterior
+res_df <- res |>
+  as_draws_df() |>
+  filter(.draw %in% sample(.draw, 100)) # sample 100 draws
+
+## find the value (x) that includes 99% of the cumulative density
+max_x <- max(qlnorm(0.99, meanlog = res_df$meanlog, sdlog = res_df$sdlog))
+
+## calculate density on grid of x values
+x <- seq(0, max_x, length.out = 100)
+res_df <- res_df |>
+  crossing(x = x) |> ## add grid to data frame
+  mutate(density = dlnorm(x, meanlog, sdlog))
+
+## plot
+ggplot(res_df, aes(x = x, y = density, group = .draw)) +
+  geom_line(alpha = 0.3)
+
+# Exercise:
+#  In this session we were in the enviable situation of knowing which
+# distribution was used to generate the data. With real data, of course,
+# we don’t have this information available. Try using a different distribution
+# for inference (e.g. normal, or gamma). Do you get a good fit?
